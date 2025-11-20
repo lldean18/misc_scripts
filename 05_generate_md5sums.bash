1e35065a66ef97c4c210b066791b129c9af966c6
Laura Dean <lldean18@gmail.com>
2025-11-20 09:45:36 +0000

    adding info on code to file

--- 05_generate_md5sums.bash
+++ 05_generate_md5sums.bash
@@ -1,16 +1,24 @@
 #!/bin/bash
+# Laura Dean
+# 19/11/25
+
+# Code to generate md5 checksum files for forward and reverse reads separately
+# with a view to submitting the files to ENA
 
 # setup for interactive job
 conda activate tmux
 tmux attach
 srun --partition defq --cpus-per-task 1 --mem 50g --time 25:00:00 --pty bash
 
-# generate forward file md5 checksums
+# move to the directory containing files you want to generate checksums for
+cd /gpfs01/home/mbzlld/data/stickleback/cist_fqs
+
+# generate forward file md5 checksums and save to file fwd.chk
 md5sum *R1* > fwd.chk
 
-# generate reverse file md5 checksums
+# generate reverse file md5 checksums and save to file rev.chk
 md5sum *R2* > rev.chk
 
-
+# checksums can then be pasted into ENA read info spreadsheet for submission
 
 
