652a61f498be8751e5aaa0ecda1da99aab1f8859
Laura Dean <lldean18@gmail.com>
2026-03-04 10:38:36 +0000

    adding script to test filtering the reads to make the files smaller

---
+++ 14_fastq_filter.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Laura Dean
+# 4/2/26
+
+
+conda activate tmux
+tmux attach -t dogs
+srun --partition defq --cpus-per-task 2 --mem 20g --time 8:00:00 --pty bash
+
+source $HOME/.bash_profile
+conda activate seqtk
+
+# calculate the number of read pairs to retain
+GENOME_SIZE=2500000000
+COVERAGE=15
+READ_LEN=150
+
+PAIRS_NEEDED=$(echo "$GENOME_SIZE * $COVERAGE / (2 * $READ_LEN)" | bc)
+echo "keeping $PAIRS_NEEDED reads from each file"
+
+
+# set the paired file names
+fastq_fwd=/share/BioinfMSc/Hannah_resources/doggies/fastqs/SRR7120208_1.fastq.gz
+fastq_rev=/share/BioinfMSc/Hannah_resources/doggies/fastqs/SRR7120208_2.fastq.gz
+
+
+# subsample the forward and reverse reads to retain only the number of pairs we specified
+seqtk sample -s100 $fastq_fwd $PAIRS_NEEDED | gzip > /gpfs01/home/mbzlld/data/dogs/filtered_fastqs/${fastq_fwd##*/}
+seqtk sample -s100 $fastq_rev $PAIRS_NEEDED | gzip > /gpfs01/home/mbzlld/data/dogs/filtered_fastqs/${fastq_rev##*/}
+
+
