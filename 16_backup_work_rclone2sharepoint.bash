4b5f5e040f1d71016d1def9c0ac06105cb2937af
Laura Dean <lldean18@gmail.com>
2026-06-04 14:04:33 +0100

    updating to backup more stuff so it can be removed

--- 16_backup_work_rclone2sharepoint.bash
+++ 16_backup_work_rclone2sharepoint.bash
@@ -15,20 +15,25 @@
 module load rclone-uon/1.65.2
 
 # copy the directory with rclone
-rclone --transfers 4 --checkers 4 --bwlimit 100M --onedrive-chunk-size 5M \
---checksum copy /gpfs01/home/mbzlld/data/bryant Laura:HPC_data_dirs_backup/bryant
+rclone --transfers 4 --checkers 4 --bwlimit 100M --onedrive-chunk-size 5M --copy-links \
+--checksum copy /gpfs01/home/mbzlld/data/splice_variant_search Laura2:HPC_data_dirs_backup/splice_variant_search
 
 # Check the directory has copied successfully
-rclone check --one-way /gpfs01/home/mbzlld/data/bryant Laura:HPC_data_dirs_backup/bryant
+rclone check --one-way /gpfs01/home/mbzlld/data/splice_variant_search Laura2:HPC_data_dirs_backup/splice_variant_search
 
 # unload module
 module unload rclone-uon/1.65.2
 
 
 
-# SUCCESSFUL LONGTERM DATA BACKUPS
+# SUCCESSFUL LONGTERM DATA BACKUPS:
+
 # /gpfs01/home/mbzlld/data/heap_data Laura:HPC_data_dirs_backup/heap_data
 # ~/data/heap_data about 3GB
 
+# /gpfs01/home/mbzlld/data/bryant Laura:HPC_data_dirs_backup/bryant
 # ~/data/bryant = 58GB
 
+
+
+# ~/data/splice_variant_search 2.5TB
