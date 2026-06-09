984a4bcd5e18630a47204291c9a8ba0bfecd3282
Laura Dean <lldean18@gmail.com>
2026-06-09 16:41:13 +0100

    running more backups so more data can be deleted

--- 09_backup_group_project_data.bash
+++ 09_backup_group_project_data.bash
@@ -13,12 +13,24 @@
 module load rclone-uon/1.65.2
 
 # copy a directory called cats from the HPC to MySite
-rclone --transfers 4 --checkers 4 --bwlimit 100M --checksum copy /share/BioinfMSc/Matt_resources OrgOne:BioinfMSc2526/Matt_resources
+rclone --transfers 4 --checkers 4 --bwlimit 100M --checksum copy /share/BioinfMSc/life4141 OrgOne:BioinfMSc2526/life4141
 #rclone --transfers 4 --checkers 4 --bwlimit 100M --checksum copy /share/BioinfMSc/life4136_2526 OrgOne:BioinfMSc2526/life4136_2526
 
 # Check the directory has copied successfully
-rclone check --one-way /share/BioinfMSc/Matt_resources OrgOne:BioinfMSc2526/Matt_resources
+rclone check --one-way /share/BioinfMSc/life4141 OrgOne:BioinfMSc2526/life4141
 #rclone check --one-way /share/BioinfMSc/life4136_2526 OrgOne:BioinfMSc2526/life4136_2526
 
 # unload the software
 module unload rclone-uon/1.65.2
+
+
+# directories successfully backed up to OrgOne:BioinfMSc2526 as of June 2026
+# Bill_resources
+# Matt_resources 149 GB
+
+
+# still waiting to do
+# life4141 146 GB
+# Hannah_resources 1.4TB
+# then finally their huge working dir life4136_2526 which is likely too big
+
